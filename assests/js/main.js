1559eae48760154175f4072a4f9235dc1d535aae
MDMOURSHAD <77190784+MDMOURSHAD@users.noreply.github.com>
2021-04-28 14:24:06 +0600

    Add files via upload

---
+++ assests/js/main.js
@@ -0,0 +1,11 @@
+	
+	$(document).ready(function(){
+    
+    	// TOGGLE SEARCH BAR
+    	$('.search-main').click(function(){
+
+    		$('.search-form-main').toggleClass('active-class');
+    		$('.search-field').focus();
+    	});
+
+	});
